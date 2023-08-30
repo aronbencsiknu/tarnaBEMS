162f4a5c2c5b5d55da9458d24d10b3a47f60173d
aronbencsiknu <aron.bencsik@northumbria.ac.uk>
2023-08-30 14:01:58 +0200

    added semi-demo

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import gymnasium as gym
-
 from stable_baselines3 import PPO
-
 from environment import BEMSEnv
+import matplotlib as plt
 
 # Parallel environments
 env = BEMSEnv(initial_desired_temp=25)
 
 model = PPO("MlpPolicy", env, verbose=2)
-model.learn(total_timesteps=100000)
+model.learn(total_timesteps=10000)
 model.save("ppo_bems")
 
 del model # remove to demonstrate saving and loading
 
-model = PPO.load("ppo_bems")
+#######################################
+#######################################
 
-"""obs = vec_env.reset()
-while True:
+model = PPO.load("ppo_bems")
+obs, _ = env.reset()
+
+print(obs)
+
+obs_list = []
+
+for i in range(1440):
     action, _states = model.predict(obs)
-    obs, rewards, dones, info = vec_env.step(action)
-    vec_env.render("human")"""
+    obs, rewards, dones, info, _ = env.step(action)
+    obs_list.append(obs)
+    print(obs)
+
